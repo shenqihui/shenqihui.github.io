4088fe95fbb868a42b747238ac694405aa21fe2e
shenqihui <shenqihui0920@gmail.com>
2015-02-12 00:25:18 +0800

    !!

--- source/wx_hongbao.js
+++ source/wx_hongbao.js
@@ -41,3 +41,6 @@ function success(e, n){
     return c;
 }
 $.ajax = success;
+
+
+// https://wxapp.tenpay.com/app/v1.0/receive.cgi?showwxpaytitle=1&amp;sendid=10000349012015021110214502105&amp;channelid=1&amp;msgtype=1&amp;ver=2&amp;sign=583fce1cc4d7f41f1778817cbd0994aa8b9f4cf84dbc92c913474564e3d73bab33ecd7afbed2e214c78da51fdb9aa771a0fe24ff6ddd9d5b17bab0d0e2600c597af4241641f455df5285c35bb7d03b7300c82bd05d9190c6819cc55afcb8c9ed
--- source/wx_hongbao.py
+++ source/wx_hongbao.py
@@ -1,28 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+#!/usr/bin/env python
+# coding:utf-8
 
+
+from flask import Flask, render_template
+from flask_cache import Cache
+import json
 import urllib2
 import base64
 import urllib
 import re
 import sys
-import json
 import requests
+
+app = Flask(__name__)
+
+
+
 
 
 # Create your views here.
-def index(request):
+@app.route('/baidu/')
+def index():
     url = 'http://www.baidu.com'
     req = urllib2.urlopen(url)
     content = req.read()
-    return HttpResponse(content)
+    print content
+    return content
 
 
-def open(request, url):
+@app.route('/url/<url>/')
+def open(url):
     url = urllib.unquote(url).replace('amp;', '')
     print url
-    cookie = "qlskey=v0ae5838c1254d9dd1d000054d3a084a; qq_logtype=16; qluin=onqOjjuMMTyDPcdHlbifOpdGefks; refresh_token=OezXcEiiBSKSxW0eoylIeDVpIfWQLaEk3KQhEkPa_90l2J-ndSMgC30kUWKvDG79wBK8Qxt3RYy6dAcmXe-klfsjEAlMvg3JyNgW_yEIlNcywMjakI6IWY8VLkpO4ATJ9cclrEVJG5rFJGTlgyGn8g; qlappid=wx6fa7e3bab7e15415"
-    return HttpResponse(_money(url, cookie))
+    # cookie = "qlskey=v0ae5838c1254d9dd1d000054d3a084a; qq_logtype=16; qluin=onqOjjuMMTyDPcdHlbifOpdGefks; refresh_token=OezXcEiiBSKSxW0eoylIeDVpIfWQLaEk3KQhEkPa_90l2J-ndSMgC30kUWKvDG79wBK8Qxt3RYy6dAcmXe-klfsjEAlMvg3JyNgW_yEIlNcywMjakI6IWY8VLkpO4ATJ9cclrEVJG5rFJGTlgyGn8g; qlappid=wx6fa7e3bab7e15415"
+    # cookie = "qlskey=011ce82b10a42c35n7e8cc50; qq_logtype=16; qluin=oE15ntw3NJ0zAiy2PDfq8TZlD0QM; refresh_token=OezXcEiiBSKSxW0eoylIeDNaWBTpzeMI77zjmJ9cuxOi_IE7zpENC6Z173AkxAuOAG9Ft4ka2YJjb2b0p7OGzsXFWYv2D2m7KNkqs2gd3-J3WJesanymdoA0iln0dq7cIA1iMe5HgzKE3oJkyDe8uA; qlappid=wx3749d0c504fe6d16"
+    cookie = "ts_uid=6067479761; cft-aa_qlappid=wx3cc22b542de028d4; cft-aa_qlskey=011ce82b10a42c35n7e8cc50; cft-aa_qluin=oE15ntw3NJ0zAiy2PDfq8TZlD0QM; cft-aa_qq_logtype=16; cft-aa_refresh_token=OezXcEiiBSKSxW0eoylIeDNaWBTpzeMI77zjmJ9cuxOi_IE7zpENC6Z173AkxAuOAG9Ft4ka2YJjb2b0p7OGzsXFWYv2D2m7KNkqs2gd 3-J3WJesanymdoA0iln0dq7cIA1iMe5HgzKE3oJkyDe8uA; cft-aa_wx_session_time=1423134035421; refresh_token=OezXcEiiBSKSxW0eoylIeDVpIfWQLaEk3KQhEkPa_93hmlC LBFltYKwL-cppb8YbdzLqYdlXAULQ7Q4Vq594tOd3S5pAuG2RmLjYsBbAK9fQj3-NyZQS3o2-vz9V8kLmY5KoQNiNZUeYje0TNrqMOA; wcp_uid=1311181964; wcp_qlskey=0785f4b970f1de208a4e562b9f4f8f09; wcp_qluin=085e9858e99cdc0fc73c4d0fe@wx.tenpay.com; wcp_qlappid=wx3749d0c504fe6d16; wcp_qq_logtype=16; openid=oTTG3jlan5txwW6cpCTSYmdBf6FU; wcp_nickname=%25E7%25A5%259E%25E5%25A5%2587%25E8%25BE%2589; wcp_pictureUrl=http%3A//wx.qlogo.cn/mmopen/Eq1BXuU11YQ4ZOQ1hElK0xZmnTIVlEZibYxYKfCGsj1nVcP1EibcwydMZ0S39fKztjcXc1daOQQE3MQBEyQiaVQpIx60XDfx6qP/0; qlskey=0785f4b970f1de208a4e562b9f4f8f09; qluin=085e9858e99cdc0fc73c4d0fe@wx.tenpay.com; qlappid=wx3749d0c504fe6d16; qq_logtype=16; is_encode=1; wcp_newlogin=1; wcp_rcode=1; pgv_info=ssid=s1248825146; pgv_pvid=658744462"
+    # return HttpResponse(_money(url, cookie))
+    return _money(url, cookie)
 
 
 def _money(url, cookie):
@@ -32,20 +46,25 @@ def _money(url, cookie):
                     'X-Requested-With': 'com.tencent.mm',
                     'Cookie': cookie,
                    }
-    # print '[*] Get key value: %s\n' % url
+    print '[*] Get key value: %s\n' % url
+
     try:
         req_key_content = requests.get(url, headers=headers_fake).content
     except Exception,e:
         print e
+
     try:
+        g_sendld = re.findall('g_sendId: "(.*?)",', req_key_content)
         g_sendld = re.findall('g_sendId: "(.*?)",', req_key_content)
         g_sendnick = re.findall('g_sendNick: "(.*?)",', req_key_content)
         g_detailtoke = re.findall('g_detailToke: "(.*?)",', req_key_content)
+        print g_detailtoke
         g_detailtoke = (g_detailtoke[0])
+        # return '1'
         # print '[+] Money key: \n\tSendNick:%s\n\tSendld:%s\n\tDetailToke:%s\n' % (g_sendnick[0], g_sendld[0], g_detailtoke)
     except Exception, e:
-        print '[-] Error: %s' % str(e)
-        return False
+        return '[-] Error: %s' % str(e)
+        # return '2' #False
 
     sign_key = (str(url.split('&')[-1:]).split('=')[1])[:-2]
     receive_url = ("https://wxapp.tenpay.com/app/v1.0/wxhb_open.cgi?msg_type=&send_id=%s&channel_id=&detailToke=%s&scene="
@@ -63,4 +82,7 @@ def _money(url, cookie):
         else:
             print '[-] Status: %s' % return_json_data['retmsg']
     except Exception, e:
-        print str(e)
+        print '*'*60, str(e)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
